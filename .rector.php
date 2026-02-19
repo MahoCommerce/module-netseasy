2ed7b9b47f105248002ba85072f5c9feb2f1577c
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2026-02-19 19:20:07 +0000

    work

--- .rector.php
+++ .rector.php
@@ -14,7 +14,6 @@ use Rector\CodeQuality\Rector\NotEqual\CommonNotEqualRector;
 use Rector\CodeQuality\Rector\Ternary\SimplifyTautologyTernaryRector;
 use Rector\CodeQuality\Rector\Ternary\SwitchNegatedTernaryRector;
 use Rector\CodingStyle\Rector\ClassMethod\MakeInheritedMethodVisibilitySameAsParentRector;
-use Rector\CodingStyle\Rector\FuncCall\ConsistentImplodeRector;
 use Rector\Config\RectorConfig;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
@@ -22,13 +21,7 @@ use Rector\DeadCode\Rector\MethodCall\RemoveNullArgOnNullDefaultParamRector;
 use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
 use Rector\EarlyReturn\Rector\If_\ChangeNestedIfsToEarlyReturnRector;
 use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
-use Rector\Php71\Rector\List_\ListToArrayDestructRector;
-use Rector\Php74\Rector\Assign\NullCoalescingOperatorRector;
-use Rector\Php80\Rector\ClassConstFetch\ClassOnThisVariableObjectRector;
-use Rector\Php80\Rector\FuncCall\ClassOnObjectRector;
-use Rector\Php80\Rector\Switch_\ChangeSwitchToMatchRector;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
 return RectorConfig::configure()
@@ -53,13 +46,6 @@ return RectorConfig::configure()
         RemoveUselessVarTagRector::class,
         ChangeNestedIfsToEarlyReturnRector::class,
         RemoveAlwaysElseRector::class,
-        ConsistentImplodeRector::class,
-        ListToArrayDestructRector::class,
-        NullCoalescingOperatorRector::class,
-        ClassOnThisVariableObjectRector::class,
-        ClassOnObjectRector::class,
-        ChangeSwitchToMatchRector::class,
         AddOverrideAttributeToOverriddenMethodsRector::class,
-        ReturnNeverTypeRector::class,
         DeclareStrictTypesRector::class,
     ]);
